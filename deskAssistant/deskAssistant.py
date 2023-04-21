0e74672850aa7eb993c1011b58b20e6ae2ca2442
Blinken77YT <theonilsson2012@icloud.com>
2023-04-21 20:07:23 +0200

    🚀 The second commit of the DeskAssistant python library

--- deskAssistant/deskAssistant.py
+++ deskAssistant/deskAssistant.py
@@ -3,64 +3,66 @@ import pyttsx3 as tts
 import datetime as dt
 import webbrowser
 
-engine = tts.init()
-r = sr.Recognizer()
+class Assistant:
+    def __init__(self):
+        self.engine = tts.init()
+        self.r = sr.Recognizer()
 
-def takeCommand():
-    with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 0.7
-        audio = r.listen(source)
-        try:
-            print("Recongizing...")
-            Query = r.recognize_google(audio, language="en-US")
-        except Exception as e:
-            print(e)
-            print("Say that again please")
-            return "None"
+    def takeCommand(self):
+        with sr.Microphone() as source:
+            print("Listening...")
+            self.r.pause_threshold = 0.7
+            audio = self.r.listen(source)
+            try:
+                print("Recongizing...")
+                Query = self.r.recognize_google(audio, language="en-US")
+            except Exception as e:
+                print(e)
+                print("Say that again please")
+                return "None"
+            
+            return Query
+
+    def speak(self, audio):
+        self.engine.say(audio)
+        self.engine.runAndWait()
+
+    def tellDay(self):
+        day = dt.datetime.today().weekday() +1
+
+        Day_dict = {
+            1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'
+        }
+
+        if day in Day_dict.keys():
+            day_of_the_week = Day_dict[day]
+            print(day_of_the_week)
+            self.speak(f"It is {day_of_the_week} today!")
+
+
+    def tellTime(self):
+        time = str(dt.datetime.now())
+        print(time)
         
-        return Query
+        hour = time[11:13]
+        if "0" in hour:
+            hour = hour.split("0")
+        min = time[14:16]
+        self.speak(f"The time is {hour} hours and {min} minutes")
 
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
+    def openMail(self):
+        webbrowser.open('mailto:', new=1)
+        self.speak("I opened your mail for you.")
 
-def tellDay():
-    day = dt.datetime.today().weekday() +1
-
-    Day_dict = {
-        1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'
-    }
-
-    if day in Day_dict.keys():
-        day_of_the_week = Day_dict[day]
-        print(day_of_the_week)
-        speak(f"It is {day_of_the_week} today!")
-
-
-def tellTime():
-    time = str(dt.datetime.now())
-    print(time)
-    
-    hour = time[11:13]
-    if "0" in hour:
-        hour = hour.split("0")
-    min = time[14:16]
-    speak(f"The time is {hour} hours and {min} minutes")
-
-def openMail():
-    webbrowser.open('mailto:', new=1)
-    speak("I opened your mail for you.")
-
-def calculate(expression):
-    try:
-        result = eval(expression)
-        return str(result)
-    except:
-        return "Sorry, I couldn't perform that calculation"
+    def calculate(self, expression):
+        try:
+            result = eval(expression)
+            return str(result)
+        except:
+            return "Sorry, I couldn't perform that calculation"
 
 
-def voiceCalc():
-    expression = takeCommand()
-    result = calculate(expression)
-    speak(f"{result}")
+    def voiceCalc(self):
+        expression = self.takeCommand()
+        result = self.calculate(expression)
+        self.speak(f"{result}")
--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup, find_packages
-import codecs
-import os
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 DESCRIPTION = 'A package to help with making a virtual assistant'
 
 # Setting up
